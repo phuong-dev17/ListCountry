8f90bc7b5a071f8963c74c7dea7db9a2953e8ecd
phuong-dev17 <125834793+phuong-dev17@users.noreply.github.com>
2024-03-05 10:24:30 +0100

    modify recycler adapter

--- module206/project1/src/main/java/se/linerotech/module206/project1/CountryRecyclerViewAdapter.kt
+++ module206/project1/src/main/java/se/linerotech/module206/project1/CountryRecyclerViewAdapter.kt
@@ -9,13 +9,14 @@ import se.linerotech.module206.project1.databinding.LayoutCellBinding
 
 class CountryRecyclerViewAdapter(
     private val items: List<CountryData>,
-    private val onCellClicked: (CountryData) ->Unit
+    private val onCellClicked1: (CountryData) ->Unit,
+    private val onCellClicked2: (CountryData) ->Unit
 ) : RecyclerView.Adapter<CountryRecyclerViewAdapter.CountryViewHolder>() {
     override fun onCreateViewHolder(
         parent: ViewGroup,
         viewType: Int
     ): CountryViewHolder {
-        return CountryViewHolder.create(parent, onCellClicked)
+        return CountryViewHolder.create(parent, onCellClicked1, onCellClicked2)
     }
 
     override fun onBindViewHolder(
@@ -32,14 +33,16 @@ class CountryRecyclerViewAdapter(
 
     class CountryViewHolder(
         private val binding: LayoutCellBinding,
-        private val onCellClicked: (CountryData) -> Unit
+        private val onCellClicked1: (CountryData) -> Unit,
+        private val onCellClicked2: (CountryData) ->Unit
     ) : RecyclerView.ViewHolder(binding.root) {
         fun bind(country: CountryData) {
             binding.countryName.text = country.country
             binding.language.text = country.language
             loadImage(country.flagUrl)
 
-            binding.root.setOnClickListener{onCellClicked(country)}
+            binding.imgFlag.setOnClickListener{onCellClicked1(country)}
+            binding.countryName.setOnClickListener { onCellClicked2(country)}
 
         }
         private fun loadImage(url: String) {
@@ -49,12 +52,12 @@ class CountryRecyclerViewAdapter(
                 .into(binding.imgFlag)
         }
         companion object {
-            fun create(parent: ViewGroup, onCellClicked: (CountryData) -> Unit): CountryViewHolder {
+            fun create(parent: ViewGroup, onCellClicked1: (CountryData) -> Unit, onCellClicked2: (CountryData) -> Unit): CountryViewHolder {
                 val binding =
                     LayoutCellBinding
                         .inflate(LayoutInflater.from(parent.context), parent, false)
 
-                return CountryViewHolder(binding, onCellClicked)
+                return CountryViewHolder(binding, onCellClicked1, onCellClicked2)
             }
         }
     }
